5d81605e0aa0bc15ec0787e303582191c4fd9a44
Will <126838201+will4525@users.noreply.github.com>
2025-06-06 14:15:01 -0400

    interval function added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     var currentQuestion;
+    // Add to top, after currentQuestion;
+    var timeLeft = 10;
     var randomNumberGenerator = function (size) {
         return Math.ceil(Math.random() * size);
     }
@@ -17,16 +19,47 @@ $(document).ready(function () {
 
     currentQuestion = questionGenerator();
     $('#equation').text(currentQuestion.equation);
+
+    $('#user-input').on('keyup', function () {
+        console.log($(this).val());
+    });
+
+    var checkAnswer = function (userInput, answer) {
+        console.log(userInput === answer);
+    }
+
+    $('#user-input').on('keyup', function () {
+        checkAnswer(Number($(this).val()), currentQuestion.answer);
+    });
+
+    var renderNewQuestion = function () {
+        currentQuestion = questionGenerator();
+        $('#equation').text(currentQuestion.equation);
+    }
+
+    var checkAnswer = function (userInput, answer) {
+        if (userInput === answer) {
+            renderNewQuestion();
+            $('#user-input').val('');
+        }
+    }
+
+    $('#user-input').on('keyup', function () {
+        checkAnswer(Number($(this).val()), currentQuestion.answer);
+    });
+
+    renderNewQuestion();
+
+    setInterval(function () {
+        console.log('1 sec passed');
+    }, 1000);
+
+    var interval = setInterval(function () {
+        timeLeft--;
+        $('#time-left').text(timeLeft);
+        if (timeLeft === 0) {
+            clearInterval(interval);
+        }
+    }, 1000);
 });
 
-$('#user-input').on('keyup', function () {
-    console.log($(this).val());
-});
-
-var checkAnswer = function (userInput, answer) {
-    console.log(userInput === answer);
-}
-
-$('#user-input').on('keyup', function () {
-    checkAnswer(Number($(this).val()), currentQuestion.answer);
-});
