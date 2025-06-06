4d2b451fb0e655729b782e225ad9ae585f823b43
Will <126838201+will4525@users.noreply.github.com>
2025-06-06 13:59:48 -0400

    reeady function added

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-// index.js
-console.log("I brought my gears")
+$(document).ready(function () {
+    var currentQuestion;
+    var randomNumberGenerator = function (size) {
+        return Math.ceil(Math.random() * size);
+    }
 
-var randomNumberGenerator = function (size) {
-    return Math.ceil(Math.random() * size);
-}
+    var questionGenerator = function () {
+        var question = {};
+        var num1 = randomNumberGenerator(10);
+        var num2 = randomNumberGenerator(10);
 
-var questionGenerator = function () {
-    var question = {};
-    var num1 = randomNumberGenerator(10);
-    var num2 = randomNumberGenerator(10);
+        question.answer = num1 + num2;
+        question.equation = String(num1) + " + " + String(num2);
 
-    question.answer = num1 + num2;
-    question.equation = String(num1) + " + " + String(num2);
+        return question;
+    }
 
-    return question;
-}
-
-console.log(questionGenerator());
-console.log(questionGenerator());
-// { answer: 10, equation: '3 + 7' }
-// { answer: 9, equation: '8 + 1' }
+    currentQuestion = questionGenerator();
+    $('#equation').text(currentQuestion.equation);
+});
