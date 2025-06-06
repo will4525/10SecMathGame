1aa887550f3d4d3ba20685f694b30bcd16c84f98
Will <126838201+will4525@users.noreply.github.com>
2025-06-06 14:06:48 -0400

    check amswer function added

--- index.js
+++ index.js
@@ -18,3 +18,15 @@ $(document).ready(function () {
     currentQuestion = questionGenerator();
     $('#equation').text(currentQuestion.equation);
 });
+
+$('#user-input').on('keyup', function () {
+    console.log($(this).val());
+});
+
+var checkAnswer = function (userInput, answer) {
+    console.log(userInput === answer);
+}
+
+$('#user-input').on('keyup', function () {
+    checkAnswer(Number($(this).val()), currentQuestion.answer);
+});
